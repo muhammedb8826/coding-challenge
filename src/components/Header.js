2bfcce1dd5b77483c2a005b69a4d3ccce1d6b4e8
Muhammed Berisso <muhammedb8826@gmail.com>
2022-12-11 12:25:42 +0300

    ui-completed

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>
+        User<span style={{ color: 'lightblue' }}>Form</span>
+      </h1>
+    </div>
+  );
+}
